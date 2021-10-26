7f6be6c53f0066417c120e777c2348bacefc7cab
Matan Borenkraout <Matanbobi@gmail.com>
2021-10-26 17:20:14 +0300

    fix: upgrade react version and adapt code

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
-import React, {useEffect, useState, useCallback, unstable_useDeferredValue} from 'react';
-import './App.css';
-import SearchBox from './components/SearchBox';
-import {sendAnalyticsPing} from './helpers/utils';
-import Description from './components/Description';
-import Header from './components/Header';
-import {
-    unstable_IdlePriority,
-    unstable_runWithPriority,
-    unstable_scheduleCallback
-} from "scheduler";
-import Spinner from './components/Spinner';
-const PokemonsList = React.lazy(() => import ('./components/PokemonsList'))
+import React, { useState, startTransition } from "react";
+import "./App.css";
+import SearchBox from "./components/SearchBox";
+import { sendAnalyticsPing } from "./helpers/utils";
+import Description from "./components/Description";
+import Header from "./components/Header";
+import { unstable_IdlePriority, unstable_scheduleCallback } from "scheduler";
+import Spinner from "./components/Spinner";
+const PokemonsList = React.lazy(() => import("./components/PokemonsList"));
 
 function App() {
-    const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
+  const [deferredValue, setDeferredValue] = useState("");
 
-    const deferredInputValue = unstable_useDeferredValue(inputValue, {
-        timeoutMs: 3000
+  const onInputChange = (value) => {
+    setInputValue(value);
+    startTransition(() => {
+      setDeferredValue(value);
     });
+    unstable_scheduleCallback(unstable_IdlePriority, function () {
+      sendAnalyticsPing(value);
+    });
+  };
 
-    const onInputChange = (value) => {
-        setInputValue(value);
-
-        unstable_scheduleCallback(unstable_IdlePriority, function () {
-            sendAnalyticsPing(value);
-        });
-    };
-
-    return (
-        <div className="App">
-            <Header>Pokémons</Header>
-            <SearchBox inputValue={inputValue} onChange={onInputChange}/>
-            <React.Suspense fallback={<Spinner/>}>
-                <PokemonsList searchValue={deferredInputValue}/>
-            </React.Suspense>
-            <Description/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header>Pokémons</Header>
+      <SearchBox inputValue={inputValue} onChange={onInputChange} />
+      <React.Suspense fallback={<Spinner />}>
+        <PokemonsList searchValue={deferredValue} />
+      </React.Suspense>
+      <Description />
+    </div>
+  );
 }
 
 export default App;
