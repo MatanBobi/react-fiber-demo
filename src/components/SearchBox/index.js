f6997dc84311278571572e43768d26e3ef8cd5f5
Matan Borenkraout <matanbobi@gmail.com>
2021-11-09 19:42:34 +0200

    chore: add spinner in input

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React from "react";
+import Spinner from "../Spinner";
+const SearchBox = ({ inputValue, onChange }) => {
+  const handleChange = (event) => {
+    onChange(event.target.value);
+  };
 
-const SearchBox = ({inputValue, onChange}) => {
-    const handleChange = event => {
-        onChange(event.target.value);
-    };
-
-    return (<input
+  return (
+    <div className="search-box">
+      <input
         type="text"
         className="input"
         value={inputValue}
         onChange={handleChange}
         placeholder="Search 🔍"
-    />);
+      />
+      {/* {isLoading && (
+        <div className="input-spinner-loading">
+          <Spinner />
+        </div>
+      )} */}
+    </div>
+  );
 };
 
 export default SearchBox;
